ba67d30978efeac75760d614fabf8dbea8a9e1de
olivier-tan-ics4u1-02-25 <olivier.tan@student.tdsb.on.ca>
2025-02-04 15:15:13 +0000

    Update

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,22 +2,34 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int num9=9;
+		int num5=5;
+		System.out.println(num9/num5);
+		System.out.println(num9%num5);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\nHello Again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		boolean yes=true;
+		System.out.println(yes);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String intro= "Hello";
+		String place= "World";
+		System.out.println(intro+" "+place);
+	
 	}
 
 }
